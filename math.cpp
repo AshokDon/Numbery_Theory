9be1606efa9719df45b870c6ad73d3fddde4c8ec
Ashok <68994450+AshokDon@users.noreply.github.com>
2024-04-28 13:54:57 +0530

    Update math.cpp

--- math.cpp
+++ math.cpp
@@ -4,6 +4,41 @@
 //almost prime it can be representd as product of two primes 
 
 #include<bits/stdc++.h>
+#include<bits/stdc++.h>
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define ll long long
+
+using namespace std;
+vector<ll>Primes;
+bool isPrimes[90000001];
+void Sieve(){
+    isPrimes[0] = isPrimes[1] = true;
+    ll maxN = 90000000;
+    for(int i = 2 ; i * i <= maxN ; i++){
+        if(!isPrimes[i]){
+            for(int j = i * i ; j <= maxN ; j+=i){
+                isPrimes[j] = true;
+            }
+        }
+    }
+    for(int i = 2 ; i <= maxN ; i++){
+        if(!isPrimes[i]){
+            Primes.push_back(i);
+        }
+    }
+
+}
+ll power(ll a, ll n, ll mod){
+    ll res = 1;
+    while(n){
+        if(n & 1){
+            res = (res * a) % mod;
+        }
+        a = (a * a) % mod;
+        n >>= 1;
+    }
+    return res;
+}
 using namespace std;
 bool Isprime(int num){
 	for(int i = 2 ; i*i <= num ; i++){
